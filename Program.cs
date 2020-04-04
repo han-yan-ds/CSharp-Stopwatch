477985ed21aba6b8aa19acfd500968901a6bf359
HAN <han@hanyancode.com>
2020-04-04 10:08:04 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch myStopwatch = new Stopwatch();
+            TimeSpan mySpan;
+
+            for (int i = 0; i < 10; i++)
+            {
+                myStopwatch.Start();
+                System.Threading.Thread.Sleep(1000);
+                mySpan = myStopwatch.Stop();
+                Console.WriteLine(mySpan);
+            }
+
+            Console.WriteLine("Finished!");
+            Console.Read();
+        }
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        private TimeSpan Ticker { get; set; }
+        private DateTime StartTime { get; set; }
+        private bool _isRunning = false;
+
+        public Stopwatch()
+        {
+            Ticker = TimeSpan.Zero;
+        }
+
+        public void Start()
+        {
+            if (!_isRunning)
+            {
+                _isRunning = true;
+                StartTime = DateTime.Now;
+            }
+        }
+
+        public TimeSpan Stop()
+        {
+            if (_isRunning)
+            {
+                _isRunning = false;
+                Ticker += (DateTime.Now - StartTime);
+            }
+            return Ticker;
+        }
+    }
+}
